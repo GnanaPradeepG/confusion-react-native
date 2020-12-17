95f8cc5999c8deb670defaedd7e81e9c3b8f6c9e
GnanaPradeepG <pradeepgundluru077@gmail.com>
2020-12-18 01:27:34 +0530

    Navigation Part 3

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
-import { View, Text , StatusBar } from 'react-native';
+import { View, Text, StatusBar, ScrollView, Image } from 'react-native';
+import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
+function RenderItem(props) {
+
+    const item = props.item;
+
+    if (item != null) {
+        return (
+            <Card>
+                <Image style={{ width: 350 }} source={require('./images/uthappizza.png')} />
+                <Card.FeaturedTitle style={{color:"#000"}}>{item.name}</Card.FeaturedTitle>
+                {item.designation ? <Card.FeaturedSubtitle style={{color:"#000"}}>{item.designation}</Card.FeaturedSubtitle> : null}
+                <Text
+                    style={{ margin: 10 }}>
+                    {item.description}</Text>
+            </Card>
+        );
+    }
+    else {
+        return (<View></View>);
+    }
+}
 
 class Home extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
+        };
+    }
+
     static navigationOptions = {
-        title: 'Home'
+        title: 'Home',
     };
 
     render() {
-        return(
-            <View>
-                <Text>Home Component</Text>
-                {/* <StatusBar
-                // backgroundColor={'transparent'}
-                // translucent={true}
-            /> */}
-                </View>
+
+        return (
+            <ScrollView>
+                <RenderItem item={this.state.dishes.filter((dish) => dish.featured)[0]} />
+                <RenderItem item={this.state.promotions.filter((promo) => promo.featured)[0]} />
+                <RenderItem item={this.state.leaders.filter((leader) => leader.featured)[0]} />
+            </ScrollView>
         );
     }
 }
